0fcc2b2aa3db931ccefdbdd6c8676ec3a1c4a11e
FedorKatanaev <fkatanaev@gmail.com>
2022-12-07 17:09:33 +0100

    test

--- MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/rooms/level0Room.java
+++ MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/rooms/level0Room.java
@@ -31,10 +31,5 @@ public class level0Room extends ICRogueRoom {
         registerActor(new Cherry(this, Orientation.DOWN, new DiscreteCoordinates(6,3) ));
         registerActor(new Staff(this, Orientation.DOWN, new DiscreteCoordinates(4,3) ));
 
-    }//HELLO FROM THE OTHER WORLD
-
+    }
 }
-
-
-
-// test to do simultanious coding
