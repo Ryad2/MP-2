c648cf09207bb857fd1b5eb592ef243d6f6bc0be
Ryad2 <102453020+Ryad2@users.noreply.github.com>
2022-12-08 16:12:32 +0100

    08/12/22 16h12

--- MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/ICRogueRoom.java
+++ MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/ICRogueRoom.java
@@ -1,22 +1,47 @@
 package ch.epfl.cs107.play.game.icrogue.area;
 
 import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.Background;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.icrogue.ICRogue;
 import ch.epfl.cs107.play.game.icrogue.ICRogueBehavior;
+import ch.epfl.cs107.play.game.icrogue.actor.Connector;
+import ch.epfl.cs107.play.game.icrogue.actor.items.Cherry;
+import ch.epfl.cs107.play.game.icrogue.actor.items.Staff;
 import ch.epfl.cs107.play.io.FileSystem;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.window.Window;
+
+import java.util.List;
 
 public abstract class ICRogueRoom extends Area {
 
     public DiscreteCoordinates coordinates;// WHY THIS SHIT IS PUBLIC I LL USE IT AS PUBLIC IN CONNECTOR BUT SHOULD CHANGE IT
     private ICRogueBehavior behavior;
-    public final String behaviorName;
+
+    private List<DiscreteCoordinates > connectorsCoordinates;
+    private List<Orientation> orientations;
+    public final String behaviorName;//CHY THIS SHIT IS PUBLIC
+
 
     public ICRogueRoom(String behaviorName, DiscreteCoordinates roomCoordinates){
         this.behaviorName = behaviorName;
         this.coordinates = roomCoordinates;
     }
+
+    public ICRogueRoom(List<DiscreteCoordinates > connectorsCoordinates ,
+                List<Orientation> orientations ,
+                String behaviorName , DiscreteCoordinates roomCoordinates){
+
+        this.connectorsCoordinates=connectorsCoordinates;
+        this.orientations=orientations;
+        this.behaviorName = behaviorName;
+        this.coordinates = roomCoordinates;
+    }
+
+    /*enum W(ouest) S(south) E(est) N(north)*/
+
+
 
 
     @Override
@@ -24,21 +49,37 @@ public abstract class ICRogueRoom extends Area {
         return ICRogue.CAMERA_SCALE_FACTOR;
     }
 
+
+
+
+
     /**
      * Create the area by adding it all actors
      * called by begin method
      * Note it set the Behavior as needed !
      */
-    protected abstract void createArea();
+    protected  void createArea(){
+        for (int i=0; i<orientations.size(); i++){
+            registerActor(new Connector(this, connectorsCoordinates.get(i), orientations.get(i)));
+        }
+    }
+
 
     public String getTitle() {              // WHY IS IT OVERWRITTEN !?
         return behaviorName;
     }
 
+
+
     //CREATED FOR CONNECTOR
     public String getTitle(String behaviorName, DiscreteCoordinates coordinates) { return behaviorName + coordinates.x + coordinates.y; }
 
+
+
     public abstract DiscreteCoordinates getPlayerSpawnPosition();
+
+
+
 
     @Override
     public boolean begin(Window window, FileSystem fileSystem) {
--- MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/rooms/level0Room.java
+++ MP2-2022/src/main/java/ch/epfl/cs107/play/game/icrogue/area/level0/rooms/level0Room.java
@@ -5,6 +5,7 @@ import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.icrogue.actor.ICRoguePlayer;
 import ch.epfl.cs107.play.game.icrogue.actor.items.Cherry;
 import ch.epfl.cs107.play.game.icrogue.actor.items.Staff;
+import ch.epfl.cs107.play.game.icrogue.area.ConnectorInRoom;
 import ch.epfl.cs107.play.game.icrogue.area.ICRogueRoom;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 
@@ -27,10 +28,40 @@ public class level0Room extends ICRogueRoom {
         return new DiscreteCoordinates(5,15);
     }
 
-    protected void createArea() {
+    protected void
+
+    createArea() {
         registerActor(new Background(this, this.behaviorName));
         registerActor(new Cherry(this, Orientation.DOWN, new DiscreteCoordinates(6,3) ));
         registerActor(new Staff(this, Orientation.DOWN, new DiscreteCoordinates(4,3) ));
 
     }
+
+
+    public enum Level0Connectors implements ConnectorInRoom {
+        // ordre des attributs: position , destination , orientation
+        W(new DiscreteCoordinates(0, 4),
+                new DiscreteCoordinates(8, 5), Orientation.LEFT),
+        S(new DiscreteCoordinates(4, 0),
+                new DiscreteCoordinates(5, 8), Orientation.DOWN),
+        E(new DiscreteCoordinates(9, 4),
+                new DiscreteCoordinates(1, 5), Orientation.RIGHT),
+        N(new DiscreteCoordinates(4, 9),
+                new DiscreteCoordinates(5, 1), Orientation.UP);
+
+    }
+
+ int getIndex(){
+        int x=0;
+       return x;
+ }
+
+
+
+
+
+
+
+
+
 }
